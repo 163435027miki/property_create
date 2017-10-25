6aad4479188fa92f68304b4fc3de042c3bc293b5
163435027miki <163435027@ccalumni.meijo-u.ac.jp>
2017-10-25 17:39:09 +0900

    2017-1025

--- property作成/proprety.cpp
+++ property作成/proprety.cpp
@@ -10,37 +10,37 @@
 
 void main(){
 ////////////////初期設定11///////////////////////////////////////////////////////////
-	char property_name[128] = "simulation17-0725";
+	char property_name[128] = "simulation17-1024";
 	int Togire[10]={0,1,3,5,7,9,13,17};
 	int pixel[10] = { 0,1,3,5,7,9,13,17 };
-	char filter_name1[128] = "kernel";
+	char filter_name1[128] = "simulation17-0823";
 	int kernel_flag = 0;	//1ならkernel，それ以外ならsobel
 	int paramerter_kernel[5] = { 3,10,100 };
 	int paramerter_sobel[5] = { 3,5,7 };
 	int Rm = 3, Rg = 3, Rs = 5, Rc = 100, Cs = 10;	//Rsは0.5を5と入力
-	int image_x = 120;
-	int image_y = 120;
+	int image_x = 256;
+	int image_y = 256;
 	int Rvector_create = 0;
 	int Rvector_coordinate_x = 59;
 	int Rvector_coordinate_y = 60;
 	int kernel_size = 31;
 
 	int sd_step = 10;
-	int sd_max = 0;
+	int sd_max = 50;
 
 	//char input_image_directory[128]="..\\\\bmp\\\\\Togire\\\\90-17p-17T";	//入力画像
 	//sprintf(kenel_directory,"..\\\\simulation17-0203\\8dire_RmRg%dkRs%dkRc%dkCs%dp",Rm,Rs,Rc,Cs);		//kernel
 
-	char input_image_directory[128];	//拡張子不要
+	char input_image_directory[128]="..\\\\bmp\\\\image\\\\image4";	//拡張子不要
 	char kenel_directory[128];
 	char input_image_sd[128];
 	
 	int paramerter_number=3;
 
-	for(int z2=1;z2<=7;++z2){	//pixelの++
-	for(int z=1;z<=7;++z){		//Togireの++
+//	for(int z2=1;z2<=7;++z2){	//pixelの++
+//	for(int z=1;z<=7;++z){		//Togireの++
 
-	sprintf(input_image_directory,"..\\\\bmp\\\\\Togire\\\\15-%dp-%dT", pixel[z2], Togire[z]);
+//	sprintf(input_image_directory,"..\\\\bmp\\\\\Togire\\\\15-%dp-%dT", pixel[z2], Togire[z]);
 
 	//実行日時の表示
 	time_t now = time(NULL);
@@ -60,15 +60,16 @@ void main(){
 	char atan_directory[128];
 
 	//フォルダの作成
-	//sprintf(date_directory,"..\\property_tiger\\%2d-%02d%02d-%02d%02d%02d\\",pnow->tm_year+1900,pnow->tm_mon + 1,pnow->tm_mday,pnow->tm_hour,pnow->tm_min,pnow->tm_sec);
-
+	sprintf(date_directory,"..\\property_tiger\\%2d-%02d%02d-%02d%02d%02d\\",pnow->tm_year+1900,pnow->tm_mon + 1,pnow->tm_mday,pnow->tm_hour,pnow->tm_min,pnow->tm_sec);
+/*
 	if (kernel_flag == 1) {
 		sprintf(date_directory, "..\\property_tiger\\15-%dp-%dT\\", pixel[z2], Togire[z]);
 	}
 	else {
 		sprintf(date_directory, "..\\property_tiger\\15-%dp-%dT_sobel\\", pixel[z2], Togire[z]);
 	}
-
+	*/
+	
 	 if(_mkdir(date_directory)==0){
         printf("フォルダ %s を作成しました\n",date_directory);
     }else{
@@ -94,15 +95,17 @@ void main(){
 				 sprintf(filenemae_conv, "%sproperty_%dk_conv_sd%d.txt", property_conv, paramerter_kernel[i], sd);
 				 if ((fp_conv = fopen(filenemae_conv, "w")) == NULL) { printf("proterty_convファイルが開けません"); exit(1); }
 
-				 fprintf(fp_conv, "%d,%d,%d,%d,%d,%d,\n", image_x, image_y, Rvector_create, Rvector_coordinate_x, Rvector_coordinate_y, kernel_size);
+				// fprintf(fp_conv, "%d,%d,%d,%d,%d,%d,\n", image_x, image_y, Rvector_create, Rvector_coordinate_x, Rvector_coordinate_y, kernel_size);
 
-				 fprintf(fp_conv, "\\\\Rvector\\\\8dire_%dk\n", paramerter_kernel[i]);
+				// fprintf(fp_conv, "%s_t.bmp\n",input_image_directory);
+
+				 fprintf(fp_conv, "..\\\\Rvector\\\\8dire_%dk\n", paramerter_kernel[i]);
 
 				 sprintf(kenel_directory, "..\\\\filter\\\\%s\\\\8dire_RmRg%dkRs0.%dkRc%dkCs%dp", filter_name1, paramerter_kernel[i], Rs, Rc, Cs);
 				 //sprintf(kenel_directory, "..\\\\filter\\\\kernel\\\\%d", paramerter[i], paramerter[i]);
 
-				 // fprintf(fp_conv,"%s_sd%d.bmp\n",input_image_directory,j*sd_step);
-				 fprintf(fp_conv, "%s.bmp\n", input_image_directory);
+				 fprintf(fp_conv,"%s_sd%d.bmp\n",input_image_directory,sd);
+				 //fprintf(fp_conv, "%s.bmp\n", input_image_directory);
 				 fprintf(fp_conv, "%s\\\\V(0).txt\n", kenel_directory);
 				 fprintf(fp_conv, "%s\\\\V(45).txt\n", kenel_directory);
 				 fprintf(fp_conv, "%s\\\\V(90).txt\n", kenel_directory);
@@ -118,15 +121,19 @@ void main(){
 
 				 sprintf(filenemae_conv, "%sproperty_%d×%dsobel_conv_sd%d.txt", property_conv, paramerter_sobel[i], paramerter_sobel[i], sd);
 				 if ((fp_conv = fopen(filenemae_conv, "w")) == NULL) { printf("proterty_convファイルが開けません"); exit(1); }
-				 fprintf(fp_conv, "%d,%d,%d,%d,%d,%d,\n", image_x, image_y, Rvector_create, Rvector_coordinate_x, Rvector_coordinate_y, kernel_size);
+			//	 fprintf(fp_conv, "%d,%d,%d,%d,%d,%d,\n", image_x, image_y, Rvector_create, Rvector_coordinate_x, Rvector_coordinate_y, kernel_size);
 
-				 sprintf(kenel_directory, "..\\\\filter\\\\%d×%dsobelN", paramerter_sobel[i], paramerter_sobel[i]);
+				// sprintf(kenel_directory, "..\\\\filter\\\\%d×%dsobelN", paramerter_sobel[i], paramerter_sobel[i]);
+				  sprintf(kenel_directory, "..\\\\filter\\\\%d×%dsobel", paramerter_sobel[i], paramerter_sobel[i]);
 
-				 fprintf(fp_conv, "\\\\Rvector\\\\%d×%dsobel_under\n", paramerter_sobel[i], paramerter_sobel[i]);
+				//   fprintf(fp_conv, "%s_t.bmp\n",input_image_directory);
 
-				 // fprintf(fp_conv,"%s%d.bmp\n",input_image_directory,i*10);
+				 fprintf(fp_conv, "..\\\\Rvector\\\\%d×%dsobel_under\n", paramerter_sobel[i], paramerter_sobel[i]);
 
-				 fprintf(fp_conv, "%s.bmp\n", input_image_directory);
+				// fprintf(fp_conv,"%s_sd%d_CUBIC.bmp\n",input_image_directory,sd);
+				 fprintf(fp_conv,"%s_sd%d.bmp\n",input_image_directory,sd);
+
+				 //fprintf(fp_conv, "%s.bmp\n", input_image_directory);
 				 fprintf(fp_conv, "%s\\\\sobel0.txt\n", kenel_directory);
 				 fprintf(fp_conv, "%s\\\\sobel45.txt\n", kenel_directory);
 				 fprintf(fp_conv, "%s\\\\sobel90.txt\n", kenel_directory);
@@ -164,7 +171,7 @@ void main(){
 	*/
 	//Sleep(1000);
 
-	}
-	}
+	//}
+	//}
 
 }
